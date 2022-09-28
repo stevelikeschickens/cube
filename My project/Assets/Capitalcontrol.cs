6208a11a6becd5570daf2533af694efb9f54ff80
STUDENT Mohammad M. Akif <Mohammad.M.Akif@students.ittralee.ie>
2022-09-28 13:02:59 +0100

    crecreated a project in unity
    Signed-off-by: STUDENT Mohammad M. Akif <Mohammad.M.Akif@students.ittralee.ie>

---
+++ My project/Assets/Capitalcontrol.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Capitalcontrol : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        transform.position += Vector3.up * Time.deltaTime;
+        if (Input.GetKey(KeyCode.DownArrow))
+            transform.position += Vector3.down * Time.deltaTime;
+
+    }
+}
