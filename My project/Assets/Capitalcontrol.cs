711a58290a775ddb2f7ea833f142712e4b0c0f22
STUDENT Mohammad M. Akif <Mohammad.M.Akif@students.ittralee.ie>
2022-09-29 10:52:51 +0100

    updupdate on project
    Signed-off-by: STUDENT Mohammad M. Akif <Mohammad.M.Akif@students.ittralee.ie>

--- My project/Assets/Capitalcontrol.cs
+++ My project/Assets/Capitalcontrol.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Capitalcontrol : MonoBehaviour
-{
+{ float turningSpeed = 180;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,8 +15,49 @@ public class Capitalcontrol : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.UpArrow))
         transform.position += Vector3.up * Time.deltaTime;
+        
         if (Input.GetKey(KeyCode.DownArrow))
             transform.position += Vector3.down * Time.deltaTime;
+       
+        if (Input.GetKey(KeyCode.LeftArrow))
+            transform.position += Vector3.left * Time.deltaTime;
+      
+        if (Input.GetKey(KeyCode.RightArrow))
+            transform.position += Vector3.right * Time.deltaTime;
+        
+        if (Input.GetKey(KeyCode.W))
+            transform.position += Vector3.up * Time.deltaTime;
+        
+        if (Input.GetKey(KeyCode.S))
+            transform.position += Vector3.down * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.A))
+            transform.position += Vector3.left * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.D))
+            transform.position += Vector3.right * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.E))
+            transform.position += Vector3.forward * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.Z))
+            transform.position += Vector3.back * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.Q))
+            transform.Rotate(Vector3.up, turningSpeed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.R))
+            transform.Rotate(Vector3.down, turningSpeed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.C))
+            transform.Rotate(Vector3.right, turningSpeed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.X))
+            transform.Rotate(Vector3.left, turningSpeed * Time.deltaTime);
+
+
+
 
     }
+
 }
