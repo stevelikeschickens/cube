57ac49d7e93de27b66f4281994f73f2bd743f053
STUDENT Mohammad M. Akif <Mohammad.M.Akif@students.ittralee.ie>
2022-10-13 10:49:13 +0100

    update number 6
    Signed-off-by: STUDENT Mohammad M. Akif <Mohammad.M.Akif@students.ittralee.ie>

--- the.cube.game/Assets/Capitalcontrol.cs
+++ the.cube.game/Assets/Capitalcontrol.cs
@@ -13,35 +13,19 @@ public class Capitalcontrol : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.UpArrow))
-        transform.position += Vector3.up * Time.deltaTime;
-        
-        if (Input.GetKey(KeyCode.DownArrow))
-            transform.position += Vector3.down * Time.deltaTime;
-       
-        if (Input.GetKey(KeyCode.LeftArrow))
-            transform.position += Vector3.left * Time.deltaTime;
-      
-        if (Input.GetKey(KeyCode.RightArrow))
-            transform.position += Vector3.right * Time.deltaTime;
-        
         if (Input.GetKey(KeyCode.W))
-            transform.position += Vector3.up * Time.deltaTime;
+        transform.position += Vector3.forward * Time.deltaTime;
         
         if (Input.GetKey(KeyCode.S))
-            transform.position += Vector3.down * Time.deltaTime;
-
+            transform.position += Vector3.back * Time.deltaTime;
+       
         if (Input.GetKey(KeyCode.A))
             transform.position += Vector3.left * Time.deltaTime;
-
+      
         if (Input.GetKey(KeyCode.D))
             transform.position += Vector3.right * Time.deltaTime;
-
-        if (Input.GetKey(KeyCode.E))
-            transform.position += Vector3.forward * Time.deltaTime;
-
-        if (Input.GetKey(KeyCode.Z))
-            transform.position += Vector3.back * Time.deltaTime;
+        
+       
 
         if (Input.GetKey(KeyCode.Q))
             transform.Rotate(Vector3.up, turningSpeed * Time.deltaTime);
